bb82b780289a72e86363d1ca2488c9b891d0f411
Andrew Gilbrough <andrew@gilbrough.com>
2020-09-03 17:10:44 -0700

    added some basic structure

---
+++ src/main.rs
@@ -0,0 +1,4 @@
+pub mod services;
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/services/mod.rs
@@ -0,0 +1,11 @@
+
+#[cfg(feature="google_music")]
+pub mod google_music;
+#[cfg(feature="spotify")]
+pub mod spotify;
+#[cfg(feature="soundcloud")]
+pub mod soundcloud;
+#[cfg(feature="bandcamp")]
+pub mod bandcamp;
+#[cfg(feature="youtube")]
+pub mod youtube;
